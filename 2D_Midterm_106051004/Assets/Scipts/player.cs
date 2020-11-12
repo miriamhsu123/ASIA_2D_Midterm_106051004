9d6cd70f6319452d2c7a7f7650da66eabaea6291
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2020-11-13 03:17:08 +0800

    新增選單按鈕功能新增恭喜及失敗選單新增觸及傳送門跳出之選單新增陷阱關卡二還沒做調味料還沒做

---
+++ 2D_Midterm_106051004/Assets/Scipts/player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject congra;
+    public GameObject gameover;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "light")
+        {
+            print("觸及傳送門");
+            congra.SetActive(true);
+        }
+        
+        if (collision.tag == "trap")
+        {
+            print("南瓜泥");
+            gameover.SetActive(true);
+
+        }
+    }
+}
---
+++ 2D_Midterm_106051004/Assets/Scipts/settingManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class settingManager : MonoBehaviour
+{
+   
+    public void NextLevel()
+    {
+        print("下一關");
+    }
+
+    public void Try1Again()
+    {
+        print("再試一次Lv1");
+        SceneManager.LoadScene("level_1");
+    }
+    
+    
+    public void ReturnToMenu()
+    {
+        print("返回選單");
+        SceneManager.LoadScene("menu");
+    }
+    
+    public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+}
