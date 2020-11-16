27d5b66fc85d5d786b966a4f214c7bd4f67ddb1e
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2020-11-16 10:23:30 +0800

    第二關剩音樂跟裝飾，第一關還在考慮要不要改。我想先做過場動畫。

--- 2D_Midterm_106051004/Assets/Scipts/settingManager.cs
+++ 2D_Midterm_106051004/Assets/Scipts/settingManager.cs
@@ -7,12 +7,19 @@ public class settingManager : MonoBehaviour
     public void NextLevel()
     {
         print("下一關");
+        SceneManager.LoadScene("level_2");
     }
 
     public void Try1Again()
     {
         print("再試一次Lv1");
         SceneManager.LoadScene("level_1");
+    }
+
+    public void Try2Again()
+    {
+        print("再試一次Lv_2");
+        SceneManager.LoadScene("level_2");
     }
     
     
